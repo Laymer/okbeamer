ad6c84cc4785aa8b90cba1793ea5c8b090869f09
Laymer <igor.kopestenski@uclouvain.be>
2020-09-25 14:39:40 +0200

    light indicator

--- src/okbeamer_server.erl
+++ src/okbeamer_server.erl
@@ -10,6 +10,11 @@
 
 %% API
 -export([start_link/0]).
+-export([leds_on/0]).
+-export([leds_on/1]).
+-export([leds_off/0]).
+-export([leds_off/1]).
+-export([light_meter_mode/1]).
 
 %% gen_server callbacks
 -export([init/1 ,
@@ -20,6 +25,11 @@
          code_change/3]).
 
 -define(SERVER , ?MODULE).
+-define(gpio1_pins , [gpio1_1, gpio1_2, gpio1_3, gpio1_4]).
+-define(gpio1_pins_4 , [gpio1_1, gpio1_2, gpio1_3, gpio1_4]).
+-define(gpio1_pins_3 , [gpio1_1, gpio1_2, gpio1_3]).
+-define(gpio1_pins_2 , [gpio1_1, gpio1_2]).
+-define(gpio1_pins_1 , [gpio1_1]).
 
 -record(state , {}).
 
@@ -58,6 +68,9 @@ start_link() ->
     {stop , Reason :: term()} | ignore).
 init([]) ->
     grisp_led:color(1, green),
+
+    %% Configure slot for Board -> Pmod communication with pins set to 0
+    grisp_gpio:configure_slot(gpio1, {output_0, output_0, output_0, output_0}),
     {ok , #state{}}.
 
 %%--------------------------------------------------------------------
@@ -142,3 +155,52 @@ code_change(_OldVsn , State , _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Switches last 4 Pmod_8LD LEDs on
+%%
+%% @end
+%%--------------------------------------------------------------------
+leds_on() ->
+    _ = [ grisp_gpio:set(Pin) || Pin <- ?gpio1_pins ].
+leds_on(Pins) ->
+    _ = [ grisp_gpio:set(Pin) || Pin <- Pins ].
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Switches last 4 Pmod_8LD LEDs off
+%%
+%% @end
+%%--------------------------------------------------------------------
+leds_off() ->
+    _ = [ grisp_gpio:clear(Pin) || Pin <- ?gpio1_pins ].
+leds_off(Pins) ->
+    _ = [ grisp_gpio:clear(Pin) || Pin <- Pins ].
+    
+leds_split(0, 4) -> leds_off();
+leds_split(1, 3) -> leds_on(?gpio1_pins_1), leds_off(?gpio1_pins_3);
+leds_split(2, 2) -> leds_on(?gpio1_pins_2), leds_off(?gpio1_pins_2);
+leds_split(3, 1) -> leds_on(?gpio1_pins_3), leds_off(?gpio1_pins_1);
+leds_split(4, 0) -> leds_on().
+
+light_meter_mode(0) ->
+    ok;
+light_meter_mode(N) ->
+    leds_off(),
+    light_meter(pmod_als:percentage()),
+    % timer:sleep(10),
+    light_meter_mode(N - 1).
+
+light_meter(Percentage) when Percentage >= 0 andalso Percentage < 20 -> 
+    leds_off();
+light_meter(Percentage) when Percentage >= 20 andalso Percentage < 40 -> 
+    grisp_gpio:set(gpio1_1);
+light_meter(Percentage) when Percentage >= 40 andalso Percentage < 60 -> 
+    leds_on(?gpio1_pins_2);
+light_meter(Percentage) when Percentage >= 60 andalso Percentage < 80 -> 
+    leds_on(?gpio1_pins_3);
+light_meter(Percentage) when Percentage >= 80 andalso Percentage =< 100 -> 
+    leds_on().
